5e017a36b965ac1a3447917f5a27e55dd3ffc7fa
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-26 01:31:48 -0700

    add AnimalList, import background image and AnimalList to Main - background image rendering properly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import Main from './Components/Main/Main';
+
+import { animals } from './data';
 
 function App() {
   return (
     <div className="App">
       <Header name='Abriel'/>
+      <Main animals={animals}/>
       <Footer email='abrielcleaver@gmail.com' />
     </div>
   );
---
+++ src/Components/AnimalList/AnimalList.js
@@ -0,0 +1,9 @@
+// <AnimalList /> component should use a .map to loop through and render an <Animal /> component for each animal in the animals array passed to it from the parent.
+
+
+import React from 'react';
+import './AnimalList.css';
+
+export default function AnimalList(){
+  return <div></div>; 
+}
